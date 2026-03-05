a93cdfdcf5103d4e8b4c69f69d64ea23a1ec8ba3
Mislav <kvesic.mislav@gmail.com>
2026-03-05 16:55:07 +0100

    add user and admin view for authro fee and service contract calculators

--- app/controllers/user/calculations_controller.rb
+++ app/controllers/user/calculations_controller.rb
@@ -2,5 +2,7 @@ class User::CalculationsController < User::BaseController
   def index
     @credits = current_user.credits
     @salary_calculators = current_user.salary_calculators
+    @author_fee_calculators = current_user.author_fee_calculators
+    @service_contract_calculators = current_user.service_contract_calculators
   end
 end
