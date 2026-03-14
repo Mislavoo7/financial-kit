407c2ead35ed0a9e59310cc3cff630df437dea92
Mislav <kvesic.mislav@gmail.com>
2026-03-14 22:32:49 +0100

    add user service contract test

--- test/controllers/user/service_contract_test.rb
+++ test/controllers/user/service_contract_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class User::AuthorFeeCalculatorsControllerTest < ActionDispatch::IntegrationTest
+class User::ServiceContractCalculatorsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   def create_about_page(position = nil)
@@ -51,47 +51,47 @@ class User::AuthorFeeCalculatorsControllerTest < ActionDispatch::IntegrationTest
       lower_rate: 20
     )
 
-    @author_fee_calculator = @user.author_fee_calculators.create!(
-      valid_author_fee_calculator_params
+    @service_contract_calculator = @user.service_contract_calculators.create!(
+      valid_service_contract_calculator_params
     )
 
-    @other_user_calculator = @other_user.author_fee_calculators.create!(
-      valid_author_fee_calculator_params
+    @other_user_calculator = @other_user.service_contract_calculators.create!(
+      valid_service_contract_calculator_params
     )
 
     sign_in @user
   end
 
   test "edit loads successfully for current user's calculator" do
-    get edit_user_author_fee_calculator_path(id: @author_fee_calculator.slug)
+    get edit_user_service_contract_calculator_path(id: @service_contract_calculator.slug)
     assert_response :success
   end
 
   test "update changes calculator for current user" do
-    patch user_author_fee_calculator_path(id: @author_fee_calculator.slug), params: {
-      author_fee_calculator: {
+    patch user_service_contract_calculator_path(id: @service_contract_calculator.slug), params: {
+      service_contract_calculator: {
         amount_in_cent: 2000
       }
     }
 
     assert_redirected_to user_calculations_path
-    assert_equal 200_000, @author_fee_calculator.reload.amount_in_cent
+    assert_equal 200_000, @service_contract_calculator.reload.amount_in_cent
   end
 
   test "update does not change calculator with invalid params" do
-    old_amount = @author_fee_calculator.amount_in_cent
+    old_amount = @service_contract_calculator.amount_in_cent
 
-    patch user_author_fee_calculator_path(id: @author_fee_calculator.slug), params: {
-      author_fee_calculator: invalid_author_fee_calculator_params
+    patch user_service_contract_calculator_path(id: @service_contract_calculator.slug), params: {
+      service_contract_calculator: invalid_service_contract_calculator_params
     }
 
     assert_response :success
-    assert_equal old_amount, @author_fee_calculator.reload.amount_in_cent
+    assert_equal old_amount, @service_contract_calculator.reload.amount_in_cent
   end
 
   test "destroy removes current user's calculator" do
-    assert_difference("AuthorFeeCalculator.count", -1) do
-      delete user_author_fee_calculator_path(id: @author_fee_calculator.slug), headers: {
+    assert_difference("ServiceContractCalculator.count", -1) do
+      delete user_service_contract_calculator_path(id: @service_contract_calculator.slug), headers: {
         "HTTP_ACCEPT" => "text/javascript"
       }
     end
@@ -100,13 +100,13 @@ class User::AuthorFeeCalculatorsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "current user cannot edit another user's calculator" do
-    get edit_user_author_fee_calculator_path(id: @other_user_calculator.slug)
+    get edit_user_service_contract_calculator_path(id: @other_user_calculator.slug)
     assert_response :not_found
   end
 
   test "current user cannot update another user's calculator" do
-    patch user_author_fee_calculator_path(id: @other_user_calculator.slug), params: {
-      author_fee_calculator: {
+    patch user_service_contract_calculator_path(id: @other_user_calculator.slug), params: {
+      service_contract_calculator: {
         amount_in_cent: 2000
       }
     }
@@ -116,8 +116,8 @@ class User::AuthorFeeCalculatorsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "current user cannot destroy another user's calculator" do
-    assert_no_difference("AuthorFeeCalculator.count") do
-      delete user_author_fee_calculator_path(id: @other_user_calculator.slug), headers: {
+    assert_no_difference("ServiceContractCalculator.count") do
+      delete user_service_contract_calculator_path(id: @other_user_calculator.slug), headers: {
         "HTTP_ACCEPT" => "text/javascript"
       }
     end
@@ -127,39 +127,31 @@ class User::AuthorFeeCalculatorsControllerTest < ActionDispatch::IntegrationTest
 
   private
 
-  def valid_author_fee_calculator_params
+  def valid_service_contract_calculator_params
     {
       amount_in_cent: 1_000_000,
       calculation_type: "brut-to-net",
-      fee_type: "artist-contract",
       city_tax_rate_id: @city_tax_rate.id,
       brut_in_cent: 1_000_000,
-      contribution_base_in_cent: 450_000,
-      net_in_cent: 874_000,
-      lump_sum_ratio: 0.30,
-      lump_sum_in_cent: 300_000,
-      lump_sum_additional_ratio: 0.25,
-      lump_sum_additional_in_cent: 250_000,
+      taxation_base_in_cent: 900000,
+      employer_to_pay_in_cent: 1075000,
       first_pillar_ratio: 0.075,
-      first_pillar_in_cent: 33_750,
+      first_pillar_in_cent: 75000,
       second_pillar_ratio: 0.025,
-      second_pillar_in_cent: 11_250,
-      total_pillar_in_cent: 45_000,
+      second_pillar_in_cent: 25000,
+      total_pillar_in_cent: 100000,
       income_tax_ratio: 0.10,
-      income_tax_in_cent: 81_000,
-      taxation_base_in_cent: 405_000,
-      income_in_cent: 405_000,
+      income_tax_in_cent: 900000,
+      net_in_cent: 693000,
       health_insurance_ratio: 0.075,
-      health_insurance_in_cent: 33_750,
-      employer_to_pay_in_cent: 1_033_750
+      health_insurance_in_cent: 75000
     }
   end
 
-  def invalid_author_fee_calculator_params
+  def invalid_service_contract_calculator_params
     {
       amount_in_cent: nil,
       calculation_type: nil,
-      fee_type: nil,
       city_tax_rate_id: nil
     }
   end
