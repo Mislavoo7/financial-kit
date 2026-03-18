2b7d2c4f97f6bf04d607dbe84b22ff9706f59f92
Mislav <kvesic.mislav@gmail.com>
2026-03-19 00:52:01 +0100

    rm local_env

--- config/application.rb
+++ config/application.rb
@@ -39,11 +39,5 @@ module FinancialKit
 
     config.time_zone = "Zagreb"
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, "config", "local_env.yml")
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exist?(env_file)
-    end
   end
 end
